4101a0399ba27d0a8596dfa2d5b06b47a0fa5641
twinsant <twinsant@18ab6870-8256-11de-a106-ebce2304a7ed>
2009-08-20 05:59:45 +0000

    Fix: input mobile number will smartly converted in modem code; Add: logging debug code; Add: reading timeout tweak; Fix: send SMS code for E61; Add: config for port, encoding

--- trunk/pysms/conf_sample.py
+++ trunk/pysms/conf_sample.py
@@ -1,3 +1,3 @@
-DEBUG_MOBILE='+861381120xxxx'
-# We could retrieve Short Message Service Center via command +CSCA?
-SMSC='+8613800100500'
+DEBUG_MOBILE='+86138xxxxxxxx'
+DEBUG_PORT = '/dev/ttyACM0'
+DEBUG_ENCODING = 'cp936'
